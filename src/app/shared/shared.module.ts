5eede51b645fc6b8da9c5d9e575b586f5edf2c21
Jairo Membreño <jalbertocastillo@yahoo.com>
2021-09-28 13:44:36 -0600

    Commit hasta video 103 Seccion 8 SPA PaisesApp

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from '@angular/router';
+
 
 
 
@@ -9,7 +11,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+    
   ],
   exports :[
     SidebarComponent
